546ac3ddeaad9f50a00f929dc78bf34e3145bc07
Cristian Pignatiello <cristian2937@gmail.com>
2025-01-04 12:09:24 +0100

    feat: cluster libri

--- createDemoTable.sql
+++ createDemoTable.sql
@@ -34,6 +34,54 @@ CREATE TABLE "public".f_pietanze
 );
 
 
+CREATE TABLE "public".d_autore
+(
+    id              serial NOT NULL,
+    autore          varchar(100),
+    ts_creazione    timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_autore PRIMARY KEY (id)
+);
+
+
+CREATE TABLE "public".d_titolo
+(
+    id              serial NOT NULL,
+    titolo          varchar(100),
+    ts_creazione    timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_titolo PRIMARY KEY (id)
+);
+
+
+CREATE TABLE "public".d_editore
+(
+    id              serial NOT NULL,
+    editore         varchar(100),
+    ts_creazione    timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_editore PRIMARY KEY (id)
+);
+
+
+CREATE TABLE "public".d_genere
+(
+    id              serial NOT NULL,
+    genere          varchar(100),
+    ts_creazione    timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_genere PRIMARY KEY (id)
+);
+
+
+CREATE TABLE "public".f_libro
+(
+    id              serial NOT NULL,
+    id_autore       integer NOT NULL,
+    id_titolo       integer NOT NULL,
+    id_editore      integer,
+    id_genere       integer,
+    ts_creazione    timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_libro PRIMARY KEY (id)
+);
+
+
 ALTER TABLE "public".f_pietanze
 ADD CONSTRAINT fk_f_pietanze_d_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES "public".d_ingrediente (id);
 
@@ -42,3 +90,15 @@ ADD CONSTRAINT fk_f_pietanze_d_spezia FOREIGN KEY (id_spezia) REFERENCES "public
 
 ALTER TABLE "public".f_pietanze
 ADD CONSTRAINT fk_f_pietanze_d_quantita FOREIGN KEY (id_quantita) REFERENCES "public".d_quantita (id);
+
+ALTER TABLE "public".f_libro
+ADD CONSTRAINT fk_f_libro_autore FOREIGN KEY (id_autore) REFERENCES "public".d_autore (id);
+
+ALTER TABLE "public".f_libro
+ADD CONSTRAINT fk_f_libro_titolo FOREIGN KEY (id_titolo) REFERENCES "public".d_titolo (id);
+
+ALTER TABLE "public".f_libro
+ADD CONSTRAINT fk_f_libro_editore FOREIGN KEY (id_editore) REFERENCES "public".d_editore (id);
+
+ALTER TABLE "public".f_libro
+ADD CONSTRAINT fk_f_libro_genere FOREIGN KEY (id_genere) REFERENCES "public".d_genere (id);
